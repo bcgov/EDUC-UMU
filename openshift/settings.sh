70e31ea3a0af13caeb7154a8cbbc3fca1b898a79
Denny <Nathan.Denny@gov.bc.ca>
2019-05-27 09:48:20 -0700

    Initial commit

---
+++ openshift/settings.sh
@@ -0,0 +1,8 @@
+export PROJECT_NAMESPACE=${PROJECT_NAMESPACE: -pbuo5q-dev}
+export GIT_URI=${GIT_URI:-"https://github.com/bcgov/EDUC-UMU.git"}
+export GIT_REF=${GIT_REF:-"master"}
+
+export components="."
+export builds=""
+export images="obiee-mgmt"
+export routes=""
