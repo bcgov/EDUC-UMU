54fc3dab9fd624ef6fc503bb616584c3f025fb77
Denny <Nathan.Denny@gov.bc.ca>
2019-05-27 14:06:30 -0700

    changed class name

--- src/main/java/example/Hello.java
+++ src/main/java/example/Hello.java
@@ -2,7 +2,7 @@ package hello;
 
 import org.joda.time.LocalTime;
 
-public class HelloWorld {
+public class Hello {
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
