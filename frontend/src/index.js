8dbb310b1766cf3d2a6260573f61c128328c3486
Denny <Nathan.Denny@gov.bc.ca>
2019-06-20 10:23:01 -0700

    Added index files and styles

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { OidcProvider } from 'redux-oidc';
+
+import './app/FontAwesome';
+
+import '../styles/index.scss';
+
+// Inject the keycloak provider
