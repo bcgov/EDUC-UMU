88e6ba917bcd9f992ed7cb050fd7732e83dbae38
Denny <Nathan.Denny@gov.bc.ca>
2019-06-24 15:43:48 -0700

    Added google fonts and tables

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,20 +1,31 @@
 <template>
+    <!--
     <v-container v-if="!isAuthenticated">
         <h1>Not Logged In</h1>
     </v-container>
+    -->
+    <v-container>
+      <v-layout wrap>
+        <v-flex xs8>
+          <v-img
+            :src="require('@/assets/images/bcid-symbol.svg')"
+            class="my-3"
+            contain
+            height="150"
+            position="left"
+          ></v-img>
+        </v-flex>
+      </v-layout>
 
-      <v-container v-else>
-    <v-layout wrap>
-      <v-flex xs8>
-        <v-img
-          :src="require('@/assets/images/bcid-symbol.svg')"
-          class="my-3"
-          contain
-          height="150"
-          position="left"
-        ></v-img>
-      </v-flex>
-    </v-layout>
+      <v-layout row wrap>
+        <v-flex xs12>
+          <v-card class='sectionCard'>
+            <v-spacer></vspacer>
+            <UserTable/>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
 </template>
 
 <style>
---
+++ frontend/src/components/UserTable.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <b-table striped hover bordered :items="items"></b-table>
+    </div>
+</template>
+
+<script>
+    export default{
+        data() {
+            return {
+                fields: ['Name', 'Email', 'IDIR', 'Privileges']
+                items: [
+                    { Name: 'Nathan Denny', Email: 'nathan.denny@gov.bc.ca', IDIR: 'NDENNY', Privileges: 'EDW_Developer' },
+                    { Name: 'Frank Reynolds', Email: 'freynolds@wolfcola.eu', IDIR: 'FREYNOLDS', Privileges: 'EDW_Administrator' },
+                    { Name: 'Peter Holland', Email: 'peter.holland@gov.bc.ca', IDIR: 'PHOLLAND', Privileges: 'EDW_General' },
+                    { Name: 'Fake Name', Email: 'fake@notreal.no', IDIR: 'FNAME', Privileges: 'EDW_Superintendant' },
+                    { Name: 'John Smith', Email: 'jsmith@email.com', IDIR: 'JSMITH', Privileges: 'EDW_School_Principle' }  
+                ]
+            }
+        }
+    }
+</script>
