4f60810f3478adecd6cf8c496e218694f02d3bce
Denny <Nathan.Denny@gov.bc.ca>
2019-06-25 14:33:43 -0700

    Added additional icons

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,13 @@ import '@/plugins/vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret)
+ 
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false;
 
