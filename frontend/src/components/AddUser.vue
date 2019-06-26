a6a8bafa21e87ed547104f591759dfba47105fdc
Denny <Nathan.Denny@gov.bc.ca>
2019-06-26 14:59:51 -0700

    Added user add page

---
+++ frontend/src/components/AddUser.vue
@@ -0,0 +1,55 @@
+<template>
+    <v-container>
+        <v-layout row wrap justify-center>
+            <v-flex>
+                <v-card>
+                    <v-form
+                    ref="form"
+                    v-model="valid"
+                    lazy-validation>
+                        <v-text-field
+                            v-model="name"
+                            :rules="nameRules"
+                            label="Name"
+                            required
+                        ></v-text-field>
+                        <v-text-field
+                            v-model="idir"
+                            :rules="idirRules"
+                            label="IDIR"
+                            required
+                        ></v-text-field>
+                        <v-btn :disabled="!valid" @click="validate">Submit</v-btn>
+                    </v-form>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default{
+    data: () -> ({
+        valid: true,
+        name: '',
+        nameRules: [
+            v => !!v || 'Name is required'
+        ],
+        idir: '',
+        idirRules: [
+            v => !!v || 'IDIR is required'
+        ]
+    }),
+
+    methods: {
+        validate() {
+            if(this.$refs.form.validate()){
+                this.snackbar=true
+            }
+        }
+    }
+}
+</script>
+
+<style>
+</style>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-import Home from '@/components/Home.vue'
+import Home from '@/components/Home.vue';
+import Add_User from '@/components/AddUser.vue';
 import { AuthRoutes } from '@/utils/constants';
 import store from './store';
 
@@ -15,6 +16,11 @@ const router = new VueRouter({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/add_user',
+      name: 'add_user',
+      component: Add_User
     },
     {
       path: '*',
