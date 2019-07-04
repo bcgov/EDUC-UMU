0d8452eb119f2fd2d2fc073868e60dae43307d79
Denny <Nathan.Denny@gov.bc.ca>
2019-07-04 13:44:57 -0700

    Added authentication endpoints

---
+++ backend/src/routes/auth.js
@@ -0,0 +1,71 @@
+const config = require('config');
+const passport = require('passport');
+const router = require('express').Router();
+const {
+  body,
+  validationResult
+} = require('express-validator/check');
+
+const auth = require('../components/auth');
+
+router.get('/', (_req, res) => {
+  res.status(200).json({
+    endpoints: [
+      '/login',
+      '/logout',
+      '/token'
+    ]
+  });
+});
+
+router.use('/callback',
+  passport.authenticate('oidc', {
+    failureRedirect: 'error'
+  }),
+  (_req, res) => {
+    res.redirect(config.get('server.frontend'));
+  }
+);
+
+router.use('/error', (_req, res) => {
+  res.status(401).json({
+    message: 'Error: Unable to authenticate'
+  });
+});
+
+router.get('/login', passport.authenticate('oidc', {
+  failureRedirect: '../error'
+}));
+
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect(config.get('server.frontend'));
+});
+
+router.post('/refresh', [
+  body('refreshToken').exists()
+], async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array()
+    });
+  }
+
+  const refresh = await auth.renew(req.body.refreshToken);
+  return res.status(200).json(refresh);
+});
+
+router.use('/token', auth.removeExpired, (req, res) => {
+  if (req.user && req.user.jwt && req.user.refreshToken) {
+    res.status(200).json(req.user);
+  } else {
+    res.status(401).json({
+      message: 'Not logged in'
+    });
+  }
+});
+
+module.exports = router;
