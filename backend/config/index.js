936482f726b3ea1821e7e12d5f09eb35d6b45315
Denny <Nathan.Denny@gov.bc.ca>
2019-07-05 15:35:19 -0700

    Changed type to string

--- backend/config/index.js
+++ backend/config/index.js
@@ -9,7 +9,7 @@ nconf.overrides({
         clientID: process.env.SSO_CLIENT_ID,
         clientSecret: process.env.SSO_USER_SECRET,
         publicKey: process.env.SSO_PUBLIC_KEY,
-        discovery: https://sso-pbuo5q-tools.pathfinder.gov.bc.ca/auth/realms/obiee-umu/.well-known/openid-configuration
+        discovery: "https://sso-pbuo5q-tools.pathfinder.gov.bc.ca/auth/realms/obiee-umu/.well-known/openid-configuration"
     },
     server: {
         frontend: process.env.SERVER_FRONTEND,
