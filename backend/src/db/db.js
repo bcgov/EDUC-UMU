23d66c6eee085aac86d40bc766eea54c9966284d
Denny <Nathan.Denny@gov.bc.ca>
2019-07-08 11:17:13 -0700

    Removed duplicate tag

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,21 +1,20 @@
 var oracledb = require('oracledb');
-var config = require('../../config/index');
 
 var db = {};
 
 db.init = function(){
-    db.db = oracledb.getConnection({
+    db = oracledb.getConnection({
         user: process.env.ORACLE_USER,
         password : process.env.ORACLE_PASSWORD,
         connectString : process.env.ORACLE_CONNECT
     });
 
-    db.db.on('error', function(error){
+    db.on('error', function(error){
         logger.error(error);
         throw(error);
     });
 
-    db.db.once('open', function(){
+    db.once('open', function(){
         logger.debug("db connection established");
     });
 };
