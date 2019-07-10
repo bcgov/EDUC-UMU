e887026c0b9701e0ac0599ccf704127f7df9e070
Denny <Nathan.Denny@gov.bc.ca>
2019-07-10 12:00:42 -0700

    Added dev environment sso url

--- backend/config/index.js
+++ backend/config/index.js
@@ -9,7 +9,7 @@ nconf.overrides({
         clientID: process.env.SSO_CLIENT_ID,
         clientSecret: process.env.SSO_USER_SECRET,
         publicKey: process.env.SSO_PUBLIC_KEY,
-        discovery: "https://sso-pbuo5q-tools.pathfinder.gov.bc.ca/auth/realms/obiee-umu/.well-known/openid-configuration"
+        discovery: "https://sso-dev.pathfinder.gov.bc.ca/auth/realms/jsgbqlip/.well-known/openid-configuration"
     },
     server: {
         frontend: 'https://obiee-umu-pbuo5q-tools.pathfinder.gov.bc.ca',
