d16015d32a7e4baf3f5990d15f5ec16a157581ab
Denny <Nathan.Denny@gov.bc.ca>
2019-07-15 14:46:33 -0700

    Added status codes to responses

--- backend/src/routes/v1/db.js
+++ backend/src/routes/v1/db.js
@@ -4,44 +4,56 @@ const Database = require('../../db/Database');
 var database = new Database();
 
 dbRouter.get('/users', async(_req, res) => {
-    var response = database.selectUsers();
-    res.json(response);
+    const response = await database.selectUsers();
+    if(response instanceof Array){
+        res.status(200).json({ users : response });
+    } else{
+        res.status(500).json({ message : "Unable to retrieve users from database"});
+    }
 });
 dbRouter.put('/users', async(_req, res) => {
     let data = _req.body.data;
-    database.updateUser(data);
+    const response = await database.updateUser(data);
 });
 dbRouter.post('/users', async(_req, res) => {
     let data = _req.body.data;
-    database.createUser(data);
+    const response = await database.createUser(data);
 });
 
 
 dbRouter.get('/proxy', async(_req, res) => {
-    database.selectProxies();
-    res.json(response);
+    const response = await database.selectProxies();
+    if(response instanceof Array){
+        res.status(200).json({ proxies : response });
+    } else{
+        res.status(500).json({ message : "Unable to retrieve proxies from database"});
+    }
 });
 dbRouter.put('/proxy', async(_req, res) => {
     let data = _req.body.data;
-    database.updateProxy(data);
+    const response = await database.updateProxy(data);
 });
 dbRouter.post('/proxy', async(_req, res) => {
     let data = _req.body.data;
-    database.createProxy(data);
+    const response = await database.createProxy(data);
 });
 
 
 dbRouter.get('/roles', async(_req, res) => {
-    database.selectRole();
-    res.json(response);
+    const response = await database.selectRole();
+    if(response instanceof Array){
+        res.status(200).json({ roles : response });
+    } else {
+        res.status(500).json({ message : "Unable to retrieve roles from database"});
+    }
 });
 dbRouter.put('/roles', async(_req, res) => {
     let data = _req.body.data;
-    database.updateRole(data);
+    const response = await database.updateRole(data);
 });
 dbRouter.post('/roles', async(_req, res) => {
     let data = _req.body.data;
-    database.createRole(data);
+    const response = await database.createRole(data);
 });
 
 module.exports = dbRouter;
