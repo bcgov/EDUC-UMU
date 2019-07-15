6c8a75558305b89fb95052871814ff53e9096c1b
Denny <Nathan.Denny@gov.bc.ca>
2019-07-15 13:19:20 -0700

    fixed typo

--- backend/src/db/roles.js
+++ backend/src/db/roles.js
@@ -24,7 +24,7 @@ Roles.prototype.selectAll = function( callback){
     db.execute(`select * from :1`, [process.env.ROLES_TABLE], callback);
 };
 
-AuthUser.prototype.select = function(id, callback){
+Roles.prototype.select = function(id, callback){
     db.execute(`select * from :1 where id=:2`, [process.env.ROLES_TABLE, id], callback);
 }
 
