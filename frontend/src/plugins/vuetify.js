6875950e28c101183c0755f0632ed98975074ab8
Denny <Nathan.Denny@gov.bc.ca>
2019-07-25 15:26:19 -0700

    Removed garbage characters

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,10 +1,6 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import 'vuetify/src/styles/main.sass';
-<<<<<<< HEAD
-
-=======
->>>>>>> d08d28411a8537cb60c66234a089d8161f3c1b95
 
 Vue.use(Vuetify, {
   iconfont: 'md',
