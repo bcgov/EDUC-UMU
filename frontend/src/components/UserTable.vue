5cf7027c2a7fea1b26a1cf42fa5953dce01d3220
Denny <Nathan.Denny@gov.bc.ca>
2019-07-29 13:33:28 -0700

    Fixed pushing of objects to array

--- frontend/src/components/UserTable.vue
+++ frontend/src/components/UserTable.vue
@@ -242,7 +242,7 @@
                                                                                                                       console.log(this.items);
                                                                                                                       var tempJson = [];
                                                                                                                       tempArray.forEach(function(element, index){
-                  tempJson.push = {"system": element[0], "username": element[1], "name": element[2], "value": element[3], "authSource": element[4], "guid": element[5], "create": element[6], "update": element[7]};
+                  tempJson.push({"system": element[0], "username": element[1], "name": element[2], "value": element[3], "authSource": element[4], "guid": element[5], "create": element[6], "update": element[7]});
                 });
                 this.itemJson = tempJson;
               });
