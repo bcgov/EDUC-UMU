b251cbe89f3e90cc747c2e4db123feaa01b0d761
Denny <Nathan.Denny@gov.bc.ca>
2019-08-06 16:11:16 -0700

    Added global method

---
+++ frontend/tests/unit/globalSetup.js
@@ -0,0 +1,6 @@
+global.originalLogError = global.console.error;
+
+global.console.error = (...args) => {
+  if (args[0].includes('[Vuetify]') && args[0].includes('https://github.com/vuetifyjs/vuetify/issues/4068')) return;
+  global.originalLogError(...args);
+};
