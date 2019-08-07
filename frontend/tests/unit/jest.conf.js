a2d325c4cc44234714b1d60d15c997936fa3689f
Denny <Nathan.Denny@gov.bc.ca>
2019-08-07 13:05:39 -0700

    Replaced double quotes

--- frontend/tests/unit/jest.conf.js
+++ frontend/tests/unit/jest.conf.js
@@ -4,43 +4,43 @@ module.exports =  {
   rootDir: path.resolve(__dirname, '../../'),
   clearMocks: true,
   setupFiles: [
-    "<rootDir>/tests/unit/setup.js",
-    "<rootDir>/node_modules/regenerator-runtime/runtime"
+    '<rootDir>/tests/unit/setup.js',
+    '<rootDir>/node_modules/regenerator-runtime/runtime'
   ],
   moduleFileExtensions: [
-    "js",
-    "json",
-    "vue",
-    "jsx"
+    'js',
+    'json',
+    'vue',
+    'jsx'
   ],
   transform: {
-    ".*\\.(vue)$": "vue-jest",
-    "^.+\\.vue$": "vue-jest",
-    ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$": "jest-transform-stub",
-    "^.+\\.jsx?$": "babel-jest"
+    '.*\\.(vue)$': 'vue-jest',
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.jsx?$': 'babel-jest'
   },
   transformIgnorePatterns: [
-    "/node_modules/"
+    '/node_modules/'
   ],
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1"
+    '^@/(.*)$': '<rootDir>/src/$1'
   },
   snapshotSerializers: [
-    "jest-serializer-vue"
+    'jest-serializer-vue'
   ],
   testMatch: [
-    "**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"
+    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
   ],
-  testURL: "http://localhost/",
+  testURL: 'http://localhost/',
   watchPlugins: [
-    "jest-watch-typeahead/filename",
-    "jest-watch-typeahead/testname"
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname'
   ],
   collectCoverage: true,
   collectCoverageFrom: [
-    "src/**/*.{js,vue}",
-    "!src/main.js",
-    "!src/plugins/*.*"
+    'src/**/*.{js,vue}',
+    '!src/main.js',
+    '!src/plugins/*.*'
   ],
-  testResultsProcessor: "jest-sonar-reporter"
+  testResultsProcessor: 'jest-sonar-reporter'
 };
