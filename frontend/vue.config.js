2b2750b285b2eb8d3c726e1c553854d1ecbfdf47
Denny <Nathan.Denny@gov.bc.ca>
2019-08-12 09:04:21 -0700

    Exposed port for selenium testing

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,6 +3,7 @@ module.exports = {
   // When running in VueCLI development mode (npm run serve) proxy calls through the intended backend API route
   // To override, see .env.development file
   devServer: {
+    host: "localhost",
     proxy: {
     // Using reduce syntax to represent multiple mount points
     // https://github.com/vuejs/vue-cli/issues/2285#issuecomment-462061125
