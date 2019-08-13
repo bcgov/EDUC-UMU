cc0b4eb8c54afea498ab9b4867d6f2de91c3e9d3
Denny <Nathan.Denny@gov.bc.ca>
2019-08-13 15:55:41 -0700

    Added tests to describe groups

--- backend/tests/unit/db/Database.spec.js
+++ backend/tests/unit/db/Database.spec.js
@@ -2,17 +2,23 @@ const Database = require('../../../src/db/Database');
 
 const database = new Database();
 
-describe('userFunctions', async () => {
-    let userRes = await database.selectUsers();
-    expect(userRes).not.toHaveLength(1);//make sure we are getting an array, not a single error
+describe('userFunctions', () => {
+    test('should have more than one entry', async () => {
+        let userRes = await database.selectUsers();
+        expect(userRes).not.toHaveLength(1);
+    });//make sure we are getting an array, not a single error
 });
 
-describe('roleFunctions', async () => {
-    let roleRes = await database.selectRole();
-    expect(roleRes).not.toHaveLength(1);
+describe('roleFunctions', () => {
+    test('should have more than one role', async () => {
+        let roleRes = await database.selectRole();
+        expect(roleRes).not.toHaveLength(1);
+    });
 });
 
-describe('proxyFunctions', async () => {
-    let proxyRes = await database.selectProxies();
-    expect(proxyRes).not.toHaveLength(1);
+describe('proxyFunctions', () => {
+    test('should hae more than one proxy', async () => {
+        let proxyRes = await database.selectProxies();
+        expect(proxyRes).not.toHaveLength(1);
+    });
 });
