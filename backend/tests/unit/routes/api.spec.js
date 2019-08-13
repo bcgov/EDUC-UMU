791d712f2d132422a0b1094707c0db1df283195b
Denny <Nathan.Denny@gov.bc.ca>
2019-08-13 12:23:01 -0700

    Fixed indentation

--- backend/tests/unit/routes/api.spec.js
+++ backend/tests/unit/routes/api.spec.js
@@ -3,16 +3,16 @@ const request = require('supertest');
 const app = require('../../../src/app');
 
 describe('/api/main', () => {
-    it('should return all available endpoints', async () => {
-        const response = await request(app).get('/api/v1');
-    
-        expect(response.statusCode).toBe(200);
-        expect(response.body).toBeTruthy();
-        expect(response.body).toEqual({
-          endpoints: [
-            '/database',
-            '/validation'
-          ]
-        });
-      });
+  it('should return all available endpoints', async () => {
+    const response = await request(app).get('/api/v1');
+  
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toBeTruthy();
+    expect(response.body).toEqual({
+      endpoints: [
+        '/database',
+        '/validation'
+      ]
+    });
+  });
 });
