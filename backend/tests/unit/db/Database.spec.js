1eaa47b36b50a01aa2ee96129dc2a0252a1dd0ae
Denny <Nathan.Denny@gov.bc.ca>
2019-08-13 15:45:59 -0700

    Added async keyword to tests with await

--- backend/tests/unit/db/Database.spec.js
+++ backend/tests/unit/db/Database.spec.js
@@ -2,17 +2,17 @@ const Database = require('../../../src/db/Database');
 
 const database = new Database();
 
-describe('userFunctions', () => {
+describe('userFunctions', async () => {
     let userRes = await database.selectUsers();
     expect(userRes).not.toHaveLength(1);//make sure we are getting an array, not a single error
 });
 
-describe('roleFunctions', () => {
+describe('roleFunctions', async () => {
     let roleRes = await database.selectRole();
     expect(roleRes).not.toHaveLength(1);
 });
 
-describe('proxyFunctions', () => {
+describe('proxyFunctions', async () => {
     let proxyRes = await database.selectProxies();
     expect(proxyRes).not.toHaveLength(1);
 });
