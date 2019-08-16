aa52a055163712036d8d924e46c7ef6298757f36
Denny <Nathan.Denny@gov.bc.ca>
2019-08-16 09:02:22 -0700

    Override nightwatch configuration

--- frontend/nightwatch.conf.js
+++ frontend/nightwatch.conf.js
@@ -1,9 +1,13 @@
 const chromedriver = require('chromedriver');
 const gecko = require('geckodriver');
+const selenium = require('selenium-server');
 
 module.exports = {
     selenium: {
         start_process: true,
+        server_path: selenium.path,
+        host: '127.0.0.1',
+        port: 4444,
         cli_args: {
             'webdriver.chrome.driver': chromedriver.path,
             'webdriver.gecko.driver': gecko.path
@@ -29,8 +33,14 @@ module.exports = {
         firefox: {
             desiredCapabilities: {
                 browserName: 'firefox',
-                acceptInsecureCerts: true
+                acceptInsecureCerts: true,
+                alwaysMatch: {
+                    "moz:firefoxOptions": {
+                        args: ['-headless']
+                    }
+                }
             }
         }
+
     }
 };
