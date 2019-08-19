51109d1e747e606bd77616f8e90b0af7fcac5add
Denny <Nathan.Denny@gov.bc.ca>
2019-08-19 13:16:45 -0700

    Changed test url

--- frontend/tests/e2e/specs/test.js
+++ frontend/tests/e2e/specs/test.js
@@ -1,9 +1,23 @@
 module.exports = {
-    'default e2e tests' : (browser) => {
+    'Application Running' : (browser) => {
       browser
-        .url(process.env.VUE_DEV_SERVER_URL)
+        .url('https://obiee-umu-pbuo5q-dev.pathfinder.gov.bc.ca/')
         .waitForElementVisible('#app', 5000)
         .assert.elementPresent('.container')
         .end();
-    },
+    }/*,
+    'Attempt Login': (browser) => {
+        browser
+            .url('https://obiee-umu-pbuo5q-dev.pathfinder.gov.bc.ca/')
+            .waitForElementVisible('#nav-login', 5000)
+            .click('#nav-login')
+            .waitForElementVisible('#zocial-idir')
+            .click('#zocial-idir')
+            .waitForElementVisible('#user')
+            .setValue('#user', process.env.IDIR_USER)
+            .setValue('#password', process.env.IDIR_PASS)
+            .click('.btn btn-primary')
+            .waitForElementVisible('.v-tabs top-round theme--dark')
+            .end();
+    }*/
 };
