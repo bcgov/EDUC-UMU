73c6174154db25352a72a1c20ccb51823cab1b1e
Denny <Nathan.Denny@gov.bc.ca>
2019-08-19 15:07:37 -0700

    Used different method to find input boxes

--- frontend/tests/e2e/specs/test.js
+++ frontend/tests/e2e/specs/test.js
@@ -12,20 +12,12 @@ module.exports = {
             .waitForElementVisible('#nav-login', 5000)
             .click('#nav-login')
             .waitForElementVisible('#zocial-idir', 5000)
-            .click('#zocial-idir');
-            browser.element('#user', selectors.unavailable, function(result){
-                if(result.value && result.value.ELEMENT) {
-                    browser
-                    .setValue('input[name=user]', process.env.IDIR_USER)
-                    .setValue('input[name=password]', process.env.IDIR_PASS)
-                    .click('.btn btn-primary')
-                    .waitForElementVisible('.v-tabs top-round theme--dark', 5000)
-                    .end();
-                } else {
-                    browser
-                        .waitForElementVisible('.v-tabs top-round theme--dark', 5000)
-                        .end();
-                }
-            });
+            .click('#zocial-idir')
+            .waitForElementVisible('input[name=user]', 5000)
+            .setValue('input[name=user]', process.env.IDIR_USER)
+            .setValue('input[name=password]', process.env.IDIR_PASS)
+            .click('.btn btn-primary')
+            .waitForElementVisible('.v-tabs top-round theme--dark', 5000)
+            .end();
     }
 };
