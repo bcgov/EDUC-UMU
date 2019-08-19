93ec6dc04b9b4756ff7a8737550853926b518384
Denny <Nathan.Denny@gov.bc.ca>
2019-08-19 15:57:36 -0700

    Added external link test

--- frontend/tests/e2e/specs/test.js
+++ frontend/tests/e2e/specs/test.js
@@ -6,6 +6,43 @@ module.exports = {
         .assert.elementPresent('.container')
         .end();
     },
+    'Test External Links' : (browser) => {
+        browser
+            .url('https://obiee-umu-pbuo5q-dev.pathfinder.gov.bc.ca/')
+            .waitForElementVisible('#app', 5000)
+            .assert.elementPresent('.container')
+            .click('#footer-home')
+            .pause(2000)
+            .assert.elementPresent('.environment-prod')
+            .back()
+            .waitForElementVisible('#app', 5000)
+            .click('#footer-about')
+            .pause(2000)
+            .assert.elementPresent('.environment-prod')
+            .back
+            .waitForElementVisible('#app', 5000)
+            .click('#footer-disclaimer')
+            .pause(2000)
+            .assert.elementPresent('.environment-prod')
+            .back
+            .waitForElementVisible('#app', 5000)
+            .click('#footer-privacy')
+            .pause(2000)
+            .assert.elementPresent('.environment-prod')
+            .back
+            .waitForElementVisible('#app', 5000)
+            .click('#footer-accessibility')
+            .pause(2000)
+            .assert.elementPresent('.environment-prod')
+            .back
+            .waitForElementVisible('#app', 5000)
+            .click('#footer-copyright')
+            .pause(2000)
+            .assert.elementPresent('.environment-prod')
+            .back
+            .waitForElementVisible('#app', 5000)
+            .end();
+    },
     'Attempt Login': (browser) => {
         browser
             .url('https://obiee-umu-pbuo5q-dev.pathfinder.gov.bc.ca/')
