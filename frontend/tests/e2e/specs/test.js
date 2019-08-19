26a321eed09ccf1c5876a98c4b1bce13a1ed4334
Denny <Nathan.Denny@gov.bc.ca>
2019-08-19 14:54:10 -0700

    Added branches to login test

--- frontend/tests/e2e/specs/test.js
+++ frontend/tests/e2e/specs/test.js
@@ -13,14 +13,19 @@ module.exports = {
             .click('#nav-login')
             .waitForElementVisible('#zocial-idir', 5000)
             .click('#zocial-idir');
-            if(browser.assert.elementPresent('#user')){
-                browser
-                .setValue('input[name=user]', process.env.IDIR_USER)
-                .setValue('input[name=password]', process.env.IDIR_PASS)
-                .click('.btn btn-primary');
-            }
-            browser
-                .waitForElementVisible('.v-tabs top-round theme--dark', 5000)
-                .end();
+            browser.element('#user', selectors.unavailable, function(result){
+                if(result.value && result.value.ELEMENT) {
+                    browser
+                    .setValue('input[name=user]', process.env.IDIR_USER)
+                    .setValue('input[name=password]', process.env.IDIR_PASS)
+                    .click('.btn btn-primary')
+                    .waitForElementVisible('.v-tabs top-round theme--dark', 5000)
+                    .end();
+                } else {
+                    browser
+                        .waitForElementVisible('.v-tabs top-round theme--dark', 5000)
+                        .end();
+                }
+            })
     }
 };
