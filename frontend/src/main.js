6853b6234481d34722bf36ba10ed778356d971df
Denny <Nathan.Denny@gov.bc.ca>
2019-08-19 13:56:20 -0700

    Reverted to babel-polyfill

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,5 @@
 import Vue from 'vue';
 import vuetify from '@/plugins/vuetify';
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
 import App from './App.vue';
 import router from './router';
 import store from './store';
