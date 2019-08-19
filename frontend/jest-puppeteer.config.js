a981ebf1a64cc6f4fe28bb86a1037f8c56aea7b3
Denny <Nathan.Denny@gov.bc.ca>
2019-08-19 09:15:10 -0700

    added jest-pupeteer dependencies

---
+++ frontend/jest-puppeteer.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    server: {
+        command: 'node_modules/.bin/vue-cli-service serve',
+        port: 4444
+    }
+}
--- frontend/tests/e2e/specs/test.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  'default e2e tests' : (browser) => {
-    browser
-      .url(process.env.VUE_DEV_SERVER_URL)
-      .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.container')
-      .end();
-  },
-};
