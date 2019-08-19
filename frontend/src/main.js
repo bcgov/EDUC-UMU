aaff65c907e9123513f04588a73557ad4772ec42
Denny <Nathan.Denny@gov.bc.ca>
2019-08-19 13:26:57 -0700

    Use updated method for polyfill

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import vuetify from '@/plugins/vuetify';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import App from './App.vue';
 import router from './router';
 import store from './store';
