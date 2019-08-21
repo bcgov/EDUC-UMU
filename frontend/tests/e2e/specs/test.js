ca5caf7afded6bfc8fdda23c7daf87c80329c985
Denny <Nathan.Denny@gov.bc.ca>
2019-08-21 13:06:11 -0700

    Added secure url to backend build config

--- frontend/tests/e2e/specs/test.js
+++ frontend/tests/e2e/specs/test.js
@@ -16,6 +16,7 @@ module.exports = {
             .assert.urlEquals('https://www2.gov.bc.ca/gov/content/home')
             .end;/*
             These tests do not work for firefox, as GeckoDriver provides marionette error
+            Can re-enable this tests when GeckoDriver fixes this issue
             .url('https://obiee-umu-pbuo5q-dev.pathfinder.gov.bc.ca/')
             .waitForElementVisible('#app', 5000)
             .click('#footer-about')
