c2136271a298d5a5544d9869ca0a51dac8c71018
Denny <Nathan.Denny@gov.bc.ca>
2019-08-22 15:01:34 -0700

    Changed how purge css works

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -16,7 +16,7 @@ module.exports = {
     plugins: [
       new PurgecssPlugin({
         paths: glob.sync([
-          path.join(__dirname, './public/index.html'),
+          path.join(__dirname, './src/index.html'),
           path.join(__dirname, './**/*.vue'),
           path.join(__dirname, './src/**/*.js')
         ])
