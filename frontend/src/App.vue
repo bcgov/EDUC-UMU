15f4460dbee6e69470e80803212dfd24525bb5b6
Denny <Nathan.Denny@gov.bc.ca>
2019-08-28 09:40:40 -0700

    Added auto-logout to client-side

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,6 +8,7 @@
 
 <script>
 import { mapGetters } from 'vuex';
+import { AuthRoutes } from '@/utils/constants.js';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -22,6 +23,32 @@ export default {
   },
   created() {
     this.$store.dispatch('auth/getJwtToken');
+    this.inactiveTime();
+  },
+  mounted() {
+    this.inactiveTime()
+  }
+  methods: {
+    inactiveTime() {
+      document.onload = this.resetTimer;
+      document.onmousemove = this.resetTimer;
+      document.onmousedown = this.resetTimer;
+      document.ontouchstart = this.resetTimer;
+      document.onclick = this.resetTimer;
+      document.onscroll = this.resetTimer;
+      document.onkeypress = this.resetTimer;
+    },
+    logout() {
+      location.href = AuthRoutes.LOGOUT;
+    },
+    resetTimer() {
+      clearTimeout(this.interval);
+      this.interval = setTimeout(this.countDown, 10000);
+    },
+    countDown () {
+      clearTimout(this.interval);
+      this.logout();
+    }
   }
 };
 </script>
