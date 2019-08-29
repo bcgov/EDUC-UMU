ee124f26b3ec8a5119cee4e06d878d92476ab142
Denny <Nathan.Denny@gov.bc.ca>
2019-08-29 15:04:29 -0700

    Fixed adding user from group menu

--- frontend/src/components/UserTable.vue
+++ frontend/src/components/UserTable.vue
@@ -39,7 +39,7 @@
                 class="mr-2"
                 dark
                 color="#43893e"
-                @click="addUser()"
+                @click="dialog_a = true"
               >
                 <v-icon left>add_circle</v-icon>
                 Add Item
@@ -150,9 +150,9 @@
     <!-- The delete and edit user actions that are available to each row -->
       <template
         v-slot:item.action="{ item }">
-          <v-btn fab depressed small dark class="list_action" @click.stop="updateUserForm(item.system, item.username, item.name, item.value, item.authSource, item.guid)" color="#43893e"><v-icon>edit</v-icon></v-btn>
-          <v-btn fab depressed small dark class="list_action" @click.stop="deleteUser('Are you sure you want to delete this user?')" color="#d93e45"><v-icon>delete</v-icon></v-btn>
-          <v-btn fab depressed small dark class="list_action" @click.stop="selectUsername(item.username)" color="#5475a7"><v-icon>group</v-icon></v-btn>
+          <v-btn icon class="list_action" @click.stop="selectUsername(item.username)" color="#5475a7"><v-icon>group</v-icon></v-btn>
+          <v-btn icon class="list_action" @click.stop="updateUserForm(item.system, item.username, item.name, item.value, item.authSource, item.guid)" color="#43893e"><v-icon>edit</v-icon></v-btn>
+          <v-btn icon class="list_action" @click.stop="deleteUser('Are you sure you want to delete this user?')" color="#d93e45"><v-icon>delete</v-icon></v-btn>
       </template>
 
 
