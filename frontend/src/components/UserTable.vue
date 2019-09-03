29ffc79b87447b62fe0dffffd6171705dddd28e5
Denny <Nathan.Denny@gov.bc.ca>
2019-09-03 11:10:01 -0700

    moved broken function to end of list

--- frontend/src/components/UserTable.vue
+++ frontend/src/components/UserTable.vue
@@ -116,7 +116,7 @@
                     <v-card-actions>
                       <v-btn color="#003366" dark text @click="dialog_a = false">Close</v-btn>
                       <v-btn color="#003366" dark text @click="addUser()">Add</v-btn>
-                      <v-file-input chips multiple label="Add CSV File"></v-file-input>
+                      <!--<v-file-input chips multiple label="Add CSV File"></v-file-input>-->
                     </v-card-actions>
                   </v-card>
                 </v-form>
@@ -326,13 +326,6 @@ export default{
   },
   methods: {
     //validates forms
-    getSystems() {
-      (this.itemJson).forEach(function(element){
-        if((this.systemArray).includes(element.system)){
-          (this.systemArray).push(element.system);
-        }
-      });
-    },
     validate () {
       if (this.$refs.form.validate()){
         this.snackbar=true;
@@ -393,7 +386,14 @@ export default{
     deleteUser(message) {
       this.deleteMessage = message;
       this.dialog_uDelete = true;
-    },/*
+    }.
+    getSystems() {
+      (this.itemJson).forEach(function(element){
+        if((this.systemArray).includes(element.system)){
+          (this.systemArray).push(element.system);
+        }
+      });
+    }/*
     deleteGroup(){
       
     }*/
