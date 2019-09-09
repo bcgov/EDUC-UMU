7d427eafec631a83a5e10551953f167e61709723
Denny <Nathan.Denny@gov.bc.ca>
2019-09-09 10:51:36 -0700

    Added audience check to jwt

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -1,5 +1,6 @@
 /*eslint-disable*/
 'use strict';
+const passport = require('passport');
 const express = require('express');
 //const keycloak = require('../components/keycloak');
 const router = express.Router();
@@ -7,12 +8,6 @@ const router = express.Router();
 const dbRouter = require('./db_routes/db');
 
 //uses the builtin OIDC function to determine whether user is authenticated
-function isAuthenticated(req, res, next) {
-  if(req.isAuthenticated()){
-    return next();
-  }
-  res.redirect('/api/auth/login');
-}
 
 //provides routing to the database endpoints
 router.get('/', (_req, res) => {
@@ -25,6 +20,8 @@ router.get('/', (_req, res) => {
 });
 
 //ensures only authenticated users can access the database endpoints
-router.use('/database', isAuthenticated, dbRouter);
+router.use('/database', passport.authenticate('jwt', {
+  session: false
+}), dbRouter);
 
 module.exports = router;
