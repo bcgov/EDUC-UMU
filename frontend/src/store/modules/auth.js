9d1ecb1cf9acef0c4bbab1d813c52b0408f62f8f
Denny <Nathan.Denny@gov.bc.ca>
2019-09-10 09:49:13 -0700

    Added role checking to auth token

--- frontend/src/store/modules/auth.js
+++ frontend/src/store/modules/auth.js
@@ -27,16 +27,16 @@ export default {
           state.acronyms = [];
         }
 
-        /*if(state.acronyms.includes("umu-access")){
+        if(state.acronyms.includes("umu-access")){
           state.isAuthenticated = true;
           localStorage.setItem('jwtToken', token);
         }
         else{
           state.isAuthenticated = false;
           localStorage.removeItem('jwtToken');
-        }*/
-        state.isAuthenticated = true;
-        localStorage.setItem('jwtToken', token);
+        }
+        //state.isAuthenticated = true;
+        //localStorage.setItem('jwtToken', token);
       } else {
         state.acronyms = [];
         state.isAuthenticated = false;
