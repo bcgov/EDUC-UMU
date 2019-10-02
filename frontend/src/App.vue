32092ed0c3d8a6f27cdcce1662b98b93087a52ee
Denny <Nathan.Denny@gov.bc.ca>
2019-10-02 09:05:12 -0700

    Fixed font

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -28,7 +28,7 @@ export default {
 
 <style>
 .v-application {
-  font-family: 'BCSans', 'Verdana', 'Arial', 'sans-serif';
+  font-family: 'BCSans', Verdana, Arial, sans-serif;
 }
 
 .v-card--flat {
