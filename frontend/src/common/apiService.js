4df3c53befc32ca021e745e03eb87dd9d912973e
NDENNY <nathan.denny@gov.bc.ca>
2019-12-09 12:49:29 -0800

    Added json formatting to api responses

--- frontend/src/common/apiService.js
+++ frontend/src/common/apiService.js
@@ -85,7 +85,21 @@ export default {
         return response.status;
       }
       console.log(response.data);
-      return response.data;
+      const rows = response.data.rows;
+      var returnJson;
+      rows.forEach(function(element, index){
+        returnJson[index].system =  element[0],
+        returnJson[index].username = element[1],
+        returnJson[index].name = element[2],
+        returnJson[index].value = element[3],
+        returnJson[index].authSource = element[4],
+        returnJson[index].guid = element[5],
+        returnJson[index].create = element[6];
+        returnJson[index].createDate = element[7],
+        returnJson[index].update = element[8],
+        returnJson[index].updateDate = element[9]
+      });
+      return returnJson;
     } catch(e) {
       console.log(`Failed to fetch from API - ${e}`);
       throw e;
