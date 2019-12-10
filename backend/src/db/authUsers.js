a564bd5f774565762510ffb8d882a6e786babfb3
NDENNY <nathan.denny@gov.bc.ca>
2019-12-10 09:14:44 -0800

    Returning rows

--- backend/src/db/authUsers.js
+++ backend/src/db/authUsers.js
@@ -67,8 +67,8 @@ class AuthUser {
           console.error(err);
         }
       }
-      console.log('Oracle object: ' + result);
-      return result;
+      console.log('Oracle object: ' + result.rows);
+      return result.rows;
     } catch(e){
       console.log("Connection failed");
       console.error(e);
