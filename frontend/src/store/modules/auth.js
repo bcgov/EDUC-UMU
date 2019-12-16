8e20bff538b0a2b43120ffebd7ec06cd0c15ef93
NDENNY <nathan.denny@gov.bc.ca>
2019-12-16 13:57:31 -0800

    Fixed json field

--- frontend/src/store/modules/auth.js
+++ frontend/src/store/modules/auth.js
@@ -106,7 +106,7 @@ export default {
       const jwt = localStorage.getItem('jwtToken');
       const decoded = jwtDecode(jwt);
       console.log(decoded);
-      const split = (decoded.json.email).split('@');
+      const split = (decoded.email).split('@');
       console.log('User IDIR: ' + split[0]);
       return split[0];
     }
